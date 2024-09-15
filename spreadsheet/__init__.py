a8df17af686e1a6baa2c1815703bb55fcb27afee
hypo69 <181187165+hypo69@users.noreply.github.com>
2024-09-16 00:27:38 +0300

    init

---
+++ spreadsheet/__init__.py
@@ -0,0 +1,11 @@
+﻿"""! Google API """
+
+## \file ../src/goog/spreadsheet/__init__.py
+# -*- coding: utf-8 -*-
+#! /usr/share/projects/hypotez/venv/scripts python
+
+from packaging.version import Version
+from .version import __version__, __doc__, __details__ 
+
+from .spreadsheet import SpreadSheet
+from .reach_spreadsheet import ReachSpreadsheet
